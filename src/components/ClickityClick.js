c958760733d90e422e8feef3c30bc6ab3e7c2412
Kevin Huang <yellow.khaos@gmail.com>
2019-01-27 02:08:23 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,12 @@
 // Code ClickityClick Component Here
+import React, {Component} from 'react';
+
+export default class ClickityClick extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      hasBeenClicked: false
+    }
+  }
+}
